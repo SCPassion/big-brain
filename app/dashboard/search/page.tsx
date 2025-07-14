4e01617a18ccea9172e69e1db1b27669f56be931
SCPassion <bernard.cryptohk@gmail.com>
2025-07-14 21:16:21 +0200

    refactor: update DocumentCard link path and enhance SearchPage with local storage for search results
    - Changed DocumentCard link to point to the correct dashboard path.
    - Added SearchResult component for improved search result display.
    - Implemented local storage functionality to persist search results across sessions.

--- app/dashboard/search/page.tsx
+++ app/dashboard/search/page.tsx
@@ -1,47 +1,94 @@
 "use client";
 
 import SearchForm from "./SearchForm";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { api } from "@/convex/_generated/api";
 import Link from "next/link";
+import { FileIcon, NotebookPen } from "lucide-react";
+
+function SearchResult({
+  url,
+  score,
+  text,
+  type,
+}: {
+  url: string;
+  score: number;
+  text: string;
+  type: "note" | "document";
+}) {
+  return (
+    <Link href={url}>
+      <li className="bg-slate-800 rounded p-4 whitespace-pre-line hover:bg-slate-700 space-y-4">
+        <div className="flex items-center gap-2 justify-between">
+          <div className="flex items-center gap-2">
+            {type === "note" ? (
+              <NotebookPen size={20} />
+            ) : (
+              <FileIcon size={20} />
+            )}
+            <span className="text-md text-slate-400 text-xl">
+              {type === "note" ? "Note" : "Document"}
+            </span>
+          </div>
+          <div className="text-md text-slate-400">
+            Relevancy of {score.toFixed(2)}
+          </div>
+        </div>
+
+        <div className="text-md text-slate-400">
+          {text.substring(0, 500)}...
+        </div>
+      </li>
+    </Link>
+  );
+}
 
 export default function SearchPage() {
   const [results, setResults] =
     useState<typeof api.search.searchAction._returnType>(null);
+
+  useEffect(() => {
+    const searchResults = localStorage.getItem("searchResults");
+    if (searchResults) {
+      setResults(JSON.parse(searchResults));
+    }
+  }, []);
+
   return (
-    <main className="w-full space-y-8">
+    <main className="w-full space-y-8 pb-24">
       <div className="flex justify-between items-center mb-8">
         <h1 className="text-4xl font-bold">Search</h1>
       </div>
 
-      <SearchForm setResults={setResults} />
+      <SearchForm
+        setResults={(searchResults) => {
+          setResults(searchResults);
+          localStorage.setItem("searchResults", JSON.stringify(searchResults));
+        }}
+      />
 
       <ul className="flex flex-col gap-4">
         {results?.map((result) => {
           if (result.type === "note") {
             return (
-              <Link
-                href={`/dashboard/notes/${result.record._id}`}
-                key={result.record._id}
-              >
-                <li className="bg-slate-800 rounded p-4 whitespace-pre-line hover:bg-slate-700">
-                  type: Note {result.score}
-                  {result.record.text.substring(0, 500)}...
-                </li>
-              </Link>
+              <SearchResult
+                url={`/dashboard/notes/${result.record._id}`}
+                score={result.score}
+                text={result.record.text}
+                type="note"
+                key={result.record._id as string}
+              />
             );
           } else {
             return (
-              <Link
-                href={`/dashboard/documents/${result.record._id}`}
-                key={result.record._id}
-              >
-                <li className="bg-slate-800 rounded p-4 whitespace-pre-line hover:bg-slate-700">
-                  type: Document {result.score}
-                  {result.record.title}
-                  {result.record.description}
-                </li>
-              </Link>
+              <SearchResult
+                url={`/dashboard/documents/${result.record._id}`}
+                score={result.score}
+                text={result.record.title + " " + result.record.description}
+                type="document"
+                key={result.record._id as string}
+              />
             );
           }
         })}
